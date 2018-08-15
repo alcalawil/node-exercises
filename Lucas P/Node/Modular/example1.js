30a66301083ef33775337f1e52c1e7276d1f6e80
Alaple <palacios836@gmail.com>
2018-08-15 18:04:13 -0300

    Ejercicio6

--- Lucas P/Node/Modular/example1.js
+++ Lucas P/Node/Modular/example1.js
@@ -1,16 +1,18 @@
-var fs = require('fs')
+const fs = require('fs')
+const array = new Array;
 
-function Example5(directory,extention,callback)
-{
-    fs.readdir(directory,extention,function (err,list){
-        for (let index = 0; index < list.length; index++)
-        {
-            var theString="." + process.argv[3];
-            if(list[index].indexOf(theString)>=0)
-            {
-                console.log(list[index])
+
+module.exports = function Example5(directory, extention, callback) {
+    fs.readdir(directory,'utf8', function (err, list) {
+        if (err) return callback(err)
+        for (let index = 0; index < list.length; index++) {
+            let theString = "." + extention;
+            if (list[index].indexOf(theString) >= 0) {
+                array.push(list[index])
+               // console.log(list[index])
             }
-        }   
+        }
+
+        callback(err, array)
     })
-    
 }
--- Lucas P/Node/Modular/example2.js
+++ Lucas P/Node/Modular/example2.js
@@ -1,12 +1,11 @@
-var example = require('./example1');
-var dir = process.argv[2];
-var filterExtension = process.argv[3];
+const example = require('./example1');
+const dir = process.argv[2];
+const filterExtension = process.argv[3];
 
-var callback = function (err, list) {
-    if (err) throw err;
-    list.forEach(function (file) {
-        console.log(file);
-    })
-}
 
-example(dir,filterExtension,callback)
+let callback = (err, list) => { 
+    if (err) return callback(err)
+    list.forEach(element => console.log(element)) 
+};
+
+example(dir, filterExtension, callback)
