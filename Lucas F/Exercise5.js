e98299b670dd62dfd889dd672f241c711b4f869e
Lucas Funes <lucasmfunes@gmail.com>
2018-08-15 15:06:55 -0300

    Ejercicio 5

--- Lucas F/Exercise5.js
+++ Lucas F/Exercise5.js
@@ -1,3 +1,24 @@
 var fs = require('fs')
-var array
+var fileList = new Array 
+var filter = process.argv[3]
 
+function findFiles(callback){
+    fs.readdir(process.argv[2], 'utf8', function readOk(err, Files) {
+        
+        for (var i = 0; i < Files.length; i++) {
+        var extension = Files[i].endsWith('.' + filter)
+            if(extension == true){
+                fileList.push (Files[i])
+            }
+        }
+        callback()
+    });
+}
+
+function log(){
+    for (var i = 0; i < fileList.length; i++) {
+    console.log(fileList[i])
+    }
+  }
+
+findFiles(log)
